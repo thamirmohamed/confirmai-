f60f5906607c24d3cca8c889fa2015a0b6737ad7
Mohamed Thamir <thamir.mohamed07@gmail.com>
2026-08-13 12:33:17 +0100

    fix password hashing

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,13 +1,12 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["bcrypt_sha256", "bcrypt"],
     deprecated="auto"
 )
 
+
 def hash_password(password: str):
-    print("PASSWORD REÇU :", repr(password))
-    print("LONGUEUR :", len(password))
     return pwd_context.hash(password)
 
 
